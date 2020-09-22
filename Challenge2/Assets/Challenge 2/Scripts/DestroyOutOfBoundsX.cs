71ae7fbda116bcbac56ed7b73c8067b8ff894d1a
Noah Berman <noahberman14@gmail.com>
2020-09-22 09:49:19 -0600

    doing well

--- Challenge2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Challenge2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class DestroyOutOfBoundsX : MonoBehaviour
 {
-    private float leftLimit = 30;
+    private float leftLimit = -40;
     private float bottomLimit = -1;
 
     // Update is called once per frame
     void Update()
     {
         // Destroy dogs if x position less than left limit
-        if (transform.position.x > leftLimit)
+        if (transform.position.x < leftLimit)
         {
             Destroy(gameObject);
         } 
--- Challenge2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Challenge2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,22 +11,25 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 7;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    
 
+    
     // Start is called before the first frame update
     void Start()
     {
+        float spawnInterval = Random.Range(3, 5);
         InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        int ballIndex = Random.Range(0, 3);
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
 
 }
